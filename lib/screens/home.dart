e37e6d56fa32cb2d985a6576c87f36cae3ebda50
Suvradip Roy <suvro467@msn.com>
2021-07-01 19:33:20 +0530

    Bottom Navigation Bar added.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 import 'package:auto_pilot/shared/drawer/auto_pilot_drawer.dart';
 import 'package:auto_pilot/shared/globals.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,6 +19,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   late PageController _pageController;
+  int _currentIndex = 0;
+  bool isDrawerClicked = true;
 
   // PageView object
   late PageView pageView;
@@ -30,6 +35,8 @@ class _HomeScreenState extends State<HomeScreen> {
       initialPage: 4,
     );
 
+    isDrawerClicked = true;
+
     // Initialize the drawer
 
     myAutoPilotDrawer = MyAutoPilotDrawer(
@@ -37,12 +44,27 @@ class _HomeScreenState extends State<HomeScreen> {
       pageController: _pageController,
     );
 
+    BottomNavyBar bottomNavyBar;
+
     // Initialize the pageview
     pageView = PageView(
       physics: NeverScrollableScrollPhysics(),
       allowImplicitScrolling: true,
       controller: _pageController,
-      onPageChanged: (index) {},
+      onPageChanged: (index) {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          currentFocus.focusedChild?.unfocus();
+        }
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+
+        setState(() {
+          _currentIndex = index;
+        });
+      },
     );
   }
 
@@ -64,21 +86,6 @@ class _HomeScreenState extends State<HomeScreen> {
           automaticallyImplyLeading: true,
           toolbarHeight: 90,
           backgroundColor: Colors.transparent,
-          /* leading: Builder(
-            builder: (context) => Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 4.0, 0.0, 4.0),
-              width: 30.0,
-              child: IconButton(
-                icon: new Icon(
-                  Icons.menu,
-                  color: Color.fromRGBO(252, 128, 25, 1),
-                  size: 30.0,
-                ),
-                onPressed: () => Scaffold.of(context).openDrawer(),
-              ),
-            ),
-          ), */
-
           title: Padding(
             padding: const EdgeInsets.only(
               top: 0.0,
@@ -141,6 +148,154 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: SizedBox.expand(
           child: pageView,
+        ),
+        /* bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(
+            10,
+          ),
+          child: BottomNavyBar(
+            containerHeight: 70,
+            backgroundColor: Colors.transparent,
+            showElevation: false,
+            selectedIndex: _currentIndex,
+            curve: Curves.easeIn,
+            onItemSelected: (index) {
+              isDrawerClicked = false;
+              setState(() => _currentIndex = index);
+
+              if (_pageController.hasClients) {
+                if (index == 2) {
+                  if (!_scaffoldKey.currentState!.isDrawerOpen) {
+                    _scaffoldKey.currentState!.openDrawer();
+                  }
+                } else {
+                  /* _pageController.animateToPage(index,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeIn); */
+                  _pageController.jumpToPage(index);
+                }
+              }
+            },
+            items: <BottomNavyBarItem>[
+              BottomNavyBarItem(
+                activeColor: Color.fromRGBO(252, 128, 25, 1),
+                title: Text(
+                  'TASK',
+                  style: TextStyle(
+                    fontFamily: 'Proxima',
+                    fontSize: 10,
+                    color: Color.fromRGBO(252, 128, 25, 1),
+                  ),
+                ),
+                icon: SvgPicture.asset(
+                  'assets/images/task 2.svg',
+                  color: Globals.appColor,
+                  height: 30,
+                  width: 30,
+                ),
+                inactiveColor: Colors.grey,
+              ),
+              BottomNavyBarItem(
+                activeColor: Color.fromRGBO(252, 128, 25, 1),
+                title: Text(
+                  'SCORE',
+                  style: TextStyle(
+                    fontFamily: 'Proxima',
+                    fontSize: 10,
+                    color: Color.fromRGBO(252, 128, 25, 1),
+                  ),
+                ),
+                icon: SvgPicture.asset(
+                  'assets/images/score2.svg',
+                  color: Globals.appColor,
+                  height: 30,
+                  width: 30,
+                ),
+                inactiveColor: Colors.grey,
+              ),
+              BottomNavyBarItem(
+                textAlign: TextAlign.justify,
+                activeColor: Color.fromRGBO(252, 128, 25, 1),
+                title: Text(
+                  'SCORE',
+                  style: TextStyle(
+                    fontFamily: 'Proxima',
+                    fontSize: 10,
+                    color: Color.fromRGBO(252, 128, 25, 1),
+                  ),
+                ),
+                icon: SvgPicture.asset(
+                  'assets/images/calendar.svg',
+                  color: Globals.appColor,
+                  height: 30,
+                  width: 30,
+                ),
+                inactiveColor: Colors.grey,
+              ),
+            ],
+          ),
+        ), */
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (value) {
+            isDrawerClicked = false;
+            setState(() => _currentIndex = value);
+
+            if (_pageController.hasClients) {
+              if (value == 2) {
+                if (!_scaffoldKey.currentState!.isDrawerOpen) {
+                  _scaffoldKey.currentState!.openDrawer();
+                }
+              } else {
+                /* _pageController.animateToPage(index,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeIn); */
+                _pageController.jumpToPage(value);
+              }
+            }
+          },
+          elevation: 0,
+          currentIndex: 0, // this will be set when a new tab is tapped
+
+          selectedLabelStyle: GoogleFonts.alegreyaSc(
+            fontSize: 14,
+            color: HexColor('#416271'),
+            fontWeight: FontWeight.normal,
+          ),
+          unselectedLabelStyle: GoogleFonts.alegreyaSc(
+            fontSize: 14,
+            color: HexColor('#416271'),
+            fontWeight: FontWeight.normal,
+          ),
+          selectedItemColor: HexColor('#416271'),
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/task 2.svg',
+                color: Globals.appColor,
+                height: 30,
+                width: 30,
+              ),
+              label: 'TASK',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/score2.svg',
+                color: Globals.appColor,
+                height: 30,
+                width: 30,
+              ),
+              label: 'SCORE',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/calendar.svg',
+                color: Globals.appColor,
+                height: 30,
+                width: 30,
+              ),
+              label: 'CALENDAR',
+            )
+          ],
         ),
       ),
     );
