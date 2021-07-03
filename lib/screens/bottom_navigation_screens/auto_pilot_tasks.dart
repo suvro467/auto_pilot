3c55ea7df262cb7e9216d6597dcbe182d8fc77a9
Suvradip Roy <suvro467@msn.com>
2021-07-03 13:30:23 +0530

    Update auto_pilot_tasks.dart

--- lib/screens/bottom_navigation_screens/auto_pilot_tasks.dart
+++ lib/screens/bottom_navigation_screens/auto_pilot_tasks.dart
@@ -1,6 +1,5 @@
 import 'package:auto_pilot/shared/globals.dart';
 import 'package:auto_pilot/shared/widgets/auto_pilot_tabbar.dart';
-import 'package:auto_pilot/shared/widgets/custom_expansion_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
