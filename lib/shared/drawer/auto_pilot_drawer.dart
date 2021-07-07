8c0643f15b50f348ed4bde07ccae690da5ec88e8
Suvradip Roy <suvro467@msn.com>
2021-07-07 13:48:06 +0530

    Drawer changes

--- lib/shared/drawer/auto_pilot_drawer.dart
+++ lib/shared/drawer/auto_pilot_drawer.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:auto_pilot/shared/globals.dart';
 import 'package:auto_pilot/shared/presentation/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyAutoPilotDrawer extends StatefulWidget {
   GlobalKey<ScaffoldState> homeScaffoldKey;
@@ -33,13 +38,112 @@ class _MyAutoPilotDrawerState extends State<MyAutoPilotDrawer> {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              child: Container(
-                color: Colors.transparent,
-                child: Stack(children: <Widget>[]),
+            Container(
+              color: Colors.transparent,
+              child: Padding(
+                padding: EdgeInsets.only(top: 50.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            splashRadius: 22,
+                            padding: const EdgeInsets.all(0),
+                            icon: Icon(
+                              Icons.arrow_left,
+                              color: Globals.appColor,
+                              size: 36,
+                            ),
+                            onPressed: () {
+                              if (widget.homeScaffoldKey.currentState!
+                                  .isDrawerOpen) Navigator.pop(context);
+                            },
+                          ),
+                          IconButton(
+                            splashRadius: 22,
+                            padding: const EdgeInsets.all(0),
+                            icon: SvgPicture.asset(
+                              'assets/images/exit.svg',
+                              color: Globals.appColor,
+                              height: 22,
+                              width: 22,
+                            ),
+                            onPressed: () {
+                              showDialog<void>(
+                                context: context,
+                                barrierDismissible:
+                                    false, // user must tap button!
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text(
+                                      'Exit MyAutoPilot',
+                                      style: TextStyle(
+                                        fontFamily: 'Proxima',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    content: SingleChildScrollView(
+                                      child: ListBody(
+                                        children: <Widget>[
+                                          Text(
+                                            'Are you sure?',
+                                            style: TextStyle(
+                                              fontFamily: 'Proxima',
+                                              fontSize: 15,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: Text(
+                                          ' Yes ',
+                                          style: TextStyle(
+                                            fontFamily: 'Proxima',
+                                            fontSize: 16,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                        onPressed: () async {
+                                          // Show the progress dialogue.
+
+                                          if (Platform.isAndroid)
+                                            SystemChannels.platform
+                                                .invokeMethod(
+                                                    'SystemNavigator.pop');
+                                        },
+                                      ),
+                                      TextButton(
+                                        child: Text(
+                                          ' No ',
+                                          style: TextStyle(
+                                            fontFamily: 'Proxima',
+                                            fontSize: 16,
+                                            color: Globals.appColor,
+                                          ),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                          )
+                        ],
+                      )
+                    ]),
               ),
             ),
+            MyAutoPilotStyles.menuItemDivider,
             ListTile(
               title: Text(
                 'Dashboard',
