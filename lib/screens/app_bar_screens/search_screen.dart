ed9ea2689cd44aeabd4ea09e54f4c8767636ae40
Suvradip Roy <suvro467@msn.com>
2021-07-16 17:42:32 +0530

    Update search_screen.dart

--- lib/screens/app_bar_screens/search_screen.dart
+++ lib/screens/app_bar_screens/search_screen.dart
@@ -49,6 +49,9 @@ class _SearchScreenState extends State<SearchScreen> {
   bool isSelectedAssignment = false;
   bool isSelectedTaskType = false;
 
+  bool isSelectedTasksAssignedTo = false;
+  bool? isSelectedTasksAssignedBy = false;
+
   List<Map<String, dynamic>> searchResults = [];
   List<Widget> searchCards = [];
 
@@ -1248,25 +1251,26 @@ class _SearchScreenState extends State<SearchScreen> {
                                           child: ElevatedButton(
                                             onPressed: () async {
                                               if (_startFromDateController
-                                                      .text.isEmpty ||
+                                                      .text.isEmpty &&
                                                   _startToDateController
-                                                      .text.isEmpty ||
+                                                      .text.isEmpty &&
                                                   _targetFromDateController
-                                                      .text.isEmpty ||
+                                                      .text.isEmpty &&
                                                   _targetToDateController
                                                       .text.isEmpty) {
                                                 setState(() {
                                                   isSelectedDates = false;
                                                 });
-                                                ShowMessage.showFlushBar(
+                                                /* ShowMessage.showFlushBar(
                                                     context,
-                                                    'Please select all the dates.');
+                                                    'Please select all the dates.'); */
                                               } else {
                                                 setState(() {
                                                   isSelectedDates = true;
                                                 });
-                                                Navigator.pop(context);
                                               }
+
+                                              Navigator.pop(context);
                                               /* if (!_isSmsOTPValid ||
                                   _smsOTPController.text.isEmpty ||
                                   !_isEmailOTPValid ||
@@ -1420,7 +1424,173 @@ class _SearchScreenState extends State<SearchScreen> {
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () async {},
+                    onPressed: () async {
+                      await showModalBottomSheet(
+                          context: context,
+                          backgroundColor: Colors.transparent,
+                          builder: (builder) {
+                            return StatefulBuilder(builder:
+                                (BuildContext context, StateSetter setState) {
+                              return Align(
+                                alignment: Alignment.bottomCenter,
+                                child: ConstrainedBox(
+                                  constraints: BoxConstraints.tight(
+                                    Size(
+                                        MediaQuery.of(context).size.width *
+                                            90 /
+                                            100,
+                                        MediaQuery.of(context).size.height *
+                                            60 /
+                                            100),
+                                  ),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: const Radius.circular(20.0),
+                                        topRight: const Radius.circular(20.0),
+                                      ),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          margin: EdgeInsets.only(
+                                            top: 30,
+                                            left: 30,
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Add Task Assignment Filter',
+                                                style: GoogleFonts.notoSerif(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: MyAutoPilotStyles
+                                                      .appColor,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        // CheckBox
+                                        Container(
+                                          margin: EdgeInsets.only(
+                                            top: 40,
+                                            left: 0,
+                                            bottom: 30,
+                                          ),
+                                          width:
+                                              MediaQuery.of(context).size.width,
+                                          //height: 100,
+                                          child: CheckboxListTile(
+                                            activeColor: HexColor('#707070'),
+                                            title: Text(
+                                              'Tasks assigned to me',
+                                              style: GoogleFonts.notoSans(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.normal,
+                                                color: HexColor('#707070'),
+                                              ),
+                                            ),
+
+                                            checkColor: Colors.white,
+                                            /* checkColor:
+                                                HexColor('#707070'),
+                                            activeColor: Colors.white,
+                                            selectedTileColor:
+                                                HexColor('#707070'),
+                                            tileColor:
+                                                HexColor('#707070'), */
+                                            value: isSelectedTasksAssignedTo,
+                                            onChanged: (newValue) {
+                                              setState(() {
+                                                isSelectedTasksAssignedTo =
+                                                    !isSelectedTasksAssignedTo;
+                                              });
+                                            },
+                                            controlAffinity: ListTileControlAffinity
+                                                .leading, //  <-- leading Checkbox
+                                          ),
+                                        ),
+                                        Card(
+                                          margin: EdgeInsets.only(
+                                            left: 30,
+                                            right: 30,
+                                          ),
+                                          elevation: 2,
+                                          shadowColor: Colors.blueAccent,
+                                          //color: Colors.amber[100],
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.only(
+                                              topRight:
+                                                  Radius.elliptical(15, 15),
+                                              bottomRight:
+                                                  Radius.elliptical(15, 15),
+                                              topLeft:
+                                                  Radius.elliptical(15, 15),
+                                              bottomLeft:
+                                                  Radius.elliptical(15, 15),
+                                            ),
+                                          ),
+                                          clipBehavior: Clip.antiAlias,
+
+                                          child: Container(
+                                            padding: EdgeInsets.only(
+                                              left: 20,
+                                              right: 20,
+                                              top: 18,
+                                              bottom: 18,
+                                            ),
+                                            height: 80,
+                                            color: Colors.white,
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Row(
+                                                  children: [],
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.only(top: 20),
+                                          child: ConstrainedBox(
+                                            constraints:
+                                                BoxConstraints.tightFor(
+                                                    width: 150, height: 55),
+                                            child: ElevatedButton(
+                                              onPressed: () async {},
+                                              style: ElevatedButton.styleFrom(
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          30.0),
+                                                ),
+                                                primary:
+                                                    MyAutoPilotStyles.appColor,
+                                              ),
+                                              child: Text(
+                                                'SET FILTER',
+                                                style: GoogleFonts.notoSerif(
+                                                  fontSize: 17,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            });
+                          });
+                    },
                     style: ElevatedButton.styleFrom(
                       padding: EdgeInsets.only(
                         left: 10,
@@ -1447,8 +1617,8 @@ class _SearchScreenState extends State<SearchScreen> {
                           ),
                         ),
                         /* SizedBox(
-                    width: 10,
-                  ), */
+                                                                      width: 10,
+                                                                    ), */
                         Icon(
                           Icons.arrow_drop_down,
                           color: isSelectedAssignment
@@ -1487,8 +1657,8 @@ class _SearchScreenState extends State<SearchScreen> {
                           ),
                         ),
                         /* SizedBox(
-                    width: 10,
-                  ), */
+                                                                      width: 10,
+                                                                    ), */
                         Icon(
                           Icons.arrow_drop_down,
                           color: isSelectedTaskType
@@ -1595,5 +1765,84 @@ class _SearchScreenState extends State<SearchScreen> {
     }
 
     return searchCards;
+  }
+
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.blue;
+    }
+    return Colors.red;
+  }
+}
+
+class CustomCheckbox extends StatefulWidget {
+  final Function onChange;
+  final bool isChecked;
+  final double size;
+  final double iconSize;
+  final Color selectedColor;
+  final Color selectedIconColor;
+  final Color borderColor;
+  final Icon checkIcon;
+
+  CustomCheckbox(
+      {required this.isChecked,
+      required this.onChange,
+      required this.size,
+      required this.iconSize,
+      required this.selectedColor,
+      required this.selectedIconColor,
+      required this.borderColor,
+      required this.checkIcon});
+
+  @override
+  _CustomCheckboxState createState() => _CustomCheckboxState();
+}
+
+class _CustomCheckboxState extends State<CustomCheckbox> {
+  bool _isSelected = false;
+
+  @override
+  void initState() {
+    _isSelected = widget.isChecked;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isSelected = !_isSelected;
+          widget.onChange(_isSelected);
+        });
+      },
+      child: AnimatedContainer(
+        margin: EdgeInsets.all(4),
+        duration: Duration(milliseconds: 500),
+        curve: Curves.fastLinearToSlowEaseIn,
+        decoration: BoxDecoration(
+            color: _isSelected ? widget.selectedColor : Colors.transparent,
+            borderRadius: BorderRadius.circular(3.0),
+            border: Border.all(
+              color: widget.borderColor,
+              width: 1.5,
+            )),
+        width: widget.size,
+        height: widget.size,
+        child: _isSelected
+            ? Icon(
+                Icons.check,
+                color: widget.selectedIconColor,
+                size: widget.iconSize,
+              )
+            : null,
+      ),
+    );
   }
 }
